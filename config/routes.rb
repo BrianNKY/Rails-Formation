e052de6b4a75e908e664763b75089f0a51c59839
Brian Thoraval <brianprestasportsweb@gmail.com>
2018-09-05 00:40:04 +0200

    mise a jour formulaire article crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'posts/index'
   root to: 'pages#home'
   
   get '/salut(/:name)', to: 'pages#salut', as: 'salut' # Utilisation d'un paramètre optionnel avec les parenthèses en URL avec la route salut
-  get '/articles', to: 'posts#index', as: 'posts'
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
