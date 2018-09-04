cc9a6b5f6026452f526f8be5715402f190d5a607
Brian Thoraval <brianprestasportsweb@gmail.com>
2018-09-04 23:49:10 +0200

    creation articles et affichage depuis la base avec un foreach

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'posts/index'
   root to: 'pages#home'
   
   get '/salut(/:name)', to: 'pages#salut', as: 'salut' # Utilisation d'un paramètre optionnel avec les parenthèses en URL avec la route salut
+  get '/articles', to: 'posts#index', as: 'posts'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
