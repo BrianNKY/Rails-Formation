8d0a50dc72dbb9624ab386c1624ca52497ef2264
Brian Thoraval <briannky@gmail.com>
2018-09-06 10:19:45 +0200

    before_action, skip_action, around mutualisation du code dans les controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,22 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:update, :edit, :show, :destroy]
+  # Permet d'executer l'action avant les méthodes et éviter qu'on répète le code
+  # skip_before_action :veryfy_authentification_token
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to posts_path
+    @post.update(post_params) # On met à jour
+    redirect_to posts_path # On redirige
   end
 
   def new
@@ -28,17 +29,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
-
   end
 
   private
 
   def post_params
     params.require(:post).permit(:name, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
